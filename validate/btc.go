f6720a2de8e2a9b6a96a1a8d4ff9186c40bdb138
n8wb <nate@innodev.llc>
2024-08-02 09:07:56 -0700

    support testnet addrs

--- validate/btc.go
+++ validate/btc.go
@@ -8,16 +8,30 @@ import (
 )
 
 const (
-	BtcAddrRegex = `^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,64}$`
+	BtcAddrRegex        = `^(bc1|[13]|tb1|[2mn])[a-zA-HJ-NP-Z0-9]{25,64}$`
+	BtcTestnetAddrRegex = `^(tb1|[2mn])[a-zA-HJ-NP-Z0-9]{25,64}$`
+	BtcMainnetRegex     = `^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,64}$`
 )
 
 var (
-	btcAddrExp = regexp.MustCompile(BtcAddrRegex)
+	btcAddrExp        = regexp.MustCompile(BtcAddrRegex)
+	btcTestnetAddrExp = regexp.MustCompile(BtcTestnetAddrRegex)
+	btcMainnetExp     = regexp.MustCompile(BtcMainnetRegex)
 )
 
 // IsTapRoot checks if a BTC address is a taproot address
 func IsTapRoot(address string) bool {
-	return strings.HasPrefix(strings.ToLower(address), "bc1p")
+	return strings.HasPrefix(strings.ToLower(address), "bc1p") || strings.HasPrefix(strings.ToLower(address), "tb1p")
+}
+
+// IsBitcoinTestnet checks if a BTC address is a testnet address
+func IsBitcoinTestnet(address string) bool {
+	return btcTestnetAddrExp.MatchString(strings.ToLower(address))
+}
+
+// IsBitcoinMainnet checks if a BTC address is a mainnet address
+func IsBitcoinMainnet(address string) bool {
+	return btcMainnetExp.MatchString(strings.ToLower(address))
 }
 
 // GetValidBtcAddr returns a valid Bitcoin address or an error if the address is invalid.
