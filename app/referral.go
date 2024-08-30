fa396fa5877c8ceaee513b851eb0bfc6c791fb88
n8wb <nate@innodev.llc>
2024-08-30 10:49:41 -0700

    referral code generation logic

---
+++ app/referral.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"crypto/rand"
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	ReferralCodeExp     = regexp.MustCompile(`^[3-9|a-h|j-k|m|n|p|r-t|x|y]{4}-[3-9|a-h|j-k|m|n|p|r-t|x|y]{4}$`)
+	RootReferralCodeExp = regexp.MustCompile(`^z[3-9|a-h|j-k|m|n|p|r-t|x|y]{3}-[3-9|a-h|j-k|m|n|p|r-t|x|y]{4}$`)
+)
+
+// Note: Excludes ilqsvwz012
+// Special: z, 0
+const referralChars = "3456789abcdefghjkmnprtxy"
+
+func NewReferralCode() (string, error) {
+
+	// Referral codes
+	out := ""
+
+	rawCode := make([]byte, 8)
+	n, err := rand.Read(rawCode)
+	if n != 8 {
+		return "", errors.New("failed to generate referral code")
+	}
+	if err != nil {
+		return "", errors.Wrap(err, "failed to generate referral code")
+	}
+
+	for i, b := range rawCode {
+		if i == 4 {
+			out += "-"
+		}
+		out += string(referralChars[b%uint8(len(referralChars))])
+	}
+
+	return out, nil
+}
+
+// IsValidReferralCode checks if a referral/root referral code is valid according to a regex pattern
+func IsValidReferralCode(code string) bool {
+	if IsRootReferralCode(code) {
+		return RootReferralCodeExp.MatchString(code)
+	}
+	return ReferralCodeExp.MatchString(code)
+}
+
+func IsRootReferralCode(code string) bool {
+	return len(code) == 9 && code[0] == 'z'
+}
+
+// NewRootReferralCode created a new root referral code.
+// root referral codes are similiar to referral codes, but the first character is
+// always z. This means it will never validate as normal referral code.
+// They are never created by a user and nobody gets a referral bonus from them.
+func NewRootReferralCode() (string, error) {
+	// First we generate a referral code
+	code, err := NewReferralCode()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to generate root referral code")
+	}
+	code = "z" + code[1:]
+	return code, nil
+}
