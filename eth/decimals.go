9256985ff8e62820d497a4ced8d775b085698f68
n8wb <nate@innodev.llc>
2024-09-01 11:16:03 -0700

    decimal helpers

---
+++ eth/decimals.go
@@ -0,0 +1,27 @@
+package eth
+
+import (
+	"math/big"
+
+	"github.com/cornbase/common-lib/conversions"
+)
+
+func ToDecimalForm(balance *big.Int, decimals int) *big.Float {
+	pow := big.NewInt(10)
+	pow = pow.Exp(pow, big.NewInt(int64(decimals)), nil)
+
+	normalizedBalance := conversions.NewLargeFloat().SetInt(balance)
+	normalizedBalance = normalizedBalance.Quo(normalizedBalance, conversions.NewLargeFloat().SetInt(pow))
+
+	return normalizedBalance
+}
+
+func FromDecimalForm(balance *big.Float, decimals int) *big.Int {
+	pow := big.NewInt(10)
+	pow = pow.Exp(pow, big.NewInt(int64(decimals)), nil)
+
+	balance = balance.Mul(balance, big.NewFloat(0).SetInt(pow))
+
+	res, _ := balance.Int(nil)
+	return res
+}
