5f4cf98b480a910fedc5f74b41eb3a91b23ac4cf
n8wb <nate@innodev.llc>
2024-09-04 13:35:46 -0700

    sentry config

---
+++ server/config/sentry.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type SentryConfig struct {
+	Enabled         bool          `env:"SENTRY_ENABLED" env-default:"true"`
+	DSN             string        `env:"SENTRY_DSN" env-default:""`
+	EnableTrace     bool          `env:"SENTRY_ENABLE_TRACE" env-default:"true"`
+	SampleRate      float64       `env:"SENTRY_SAMPLE_RATE" env-default:"1.0"` // Set to 1.0 to capture 100% of transactions
+	Repanic         bool          `env:"SENTRY_REPANIC" env-default:"true"`
+	Timeout         time.Duration `env:"SENTRY_TIMEOUT" env-default:"2s"`
+	WaitForDelivery bool          `env:"SENTRY_WAIT_FOR_DELIVERY" env-default:"false"`
+}
+
+func (sc SentryConfig) SentryEnabled() bool {
+	return len(sc.DSN) != 0 && sc.Enabled
+}
+
+func NewSentryConfig() (SentryConfig, error) {
+	sentryConfig := SentryConfig{}
+	err := cleanenv.ReadEnv(&sentryConfig)
+	return sentryConfig, err
+}
