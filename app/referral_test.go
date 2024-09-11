938471ee0edb18bd963109ede643ec7f3d39fde5
n8wb <vanish172@gmail.com>
2024-09-11 12:48:54 -0700

    code unit tests

--- app/referral_test.go
+++ app/referral_test.go
@@ -1,6 +1,10 @@
 package app
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func Test_NewReferralCode(t *testing.T) {
 	code, err := NewReferralCode()
@@ -9,5 +13,16 @@ func Test_NewReferralCode(t *testing.T) {
 	}
 	if len(code) != 9 {
 		t.Errorf("expected referral code to be 9 characters, got %d", len(code))
+	}
+}
+
+func Test_IsValidReferralCode(t *testing.T) {
+	validCodes := []string{
+		"ipm7-ffe3",
+		"zyr7-cbn8",
+	}
+
+	for _, code := range validCodes {
+		require.Truef(t, IsValidReferralCode(code), "expected %s to be valid", code)
 	}
 }
